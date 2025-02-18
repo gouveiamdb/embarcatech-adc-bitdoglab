c5a7ed1b7327d011cb62732d1c99e5cff05f5e28
Matheus Gouveia <gouveiamdb@gmail.com>
2025-02-18 11:48:43 -0300

    Adiciona suporte para controle de exibição e debounce de botões, além de otimizar a inicialização do display

--- embarcatech-adc-bitdoglab.c
+++ embarcatech-adc-bitdoglab.c
@@ -19,24 +19,36 @@
 #define endereco 0x3C
 
 volatile bool led_green_state = false;
-volatile bool pwm_enable = true;
+volatile bool pwm_enabled = true;
 volatile uint8_t border_style = 0;
-const uint32_t DEBOUCE_DELAY = 200000;
+volatile uint32_t last_button_time = 0;
+const uint32_t DEBOUNCE_DELAY = 200000;
+ssd1306_t ssd;
+bool cor = true;
 
-void gpio_callback(uint gpio, uint32_t events)
-{
+uint8_t display_buffer[WIDTH * HEIGHT / 8] = {0};
+
+void gpio_callback(uint gpio, uint32_t events) {
+    uint32_t current_time = time_us_32();
+    
+    if (current_time - last_button_time < DEBOUNCE_DELAY) {
+        return;
+    }
+    
+    last_button_time = current_time;
+
     if (gpio == JOYSTICK_BTN) {
         led_green_state = !led_green_state;
         gpio_put(LED_GREEN, led_green_state);
         border_style = (border_style + 1) % 3;
-        }
-        else if (gpio == BUTTON_A) {
-            pwm_enable = !pwm_enable;
+        update_display();
+
+    } else if (gpio == BUTTON_A) {
+        pwm_enabled = !pwm_enabled;
     }
 }
 
-void init_gpio()
-{
+void init_gpio() {
     gpio_init(JOYSTICK_BTN);
     gpio_set_dir(JOYSTICK_BTN, GPIO_IN);
     gpio_pull_up(JOYSTICK_BTN);
@@ -61,7 +73,7 @@ void init_adc()
 
 void init_i2c()
 {
-    i2c_init(I2C_PORT, 400000);
+    i2c_init(I2C_PORT, 400 * 1000);
     gpio_set_function(I2C_SDA, GPIO_FUNC_I2C);
     gpio_set_function(I2C_SCL, GPIO_FUNC_I2C);
     gpio_pull_up(I2C_SDA);
@@ -83,6 +95,38 @@ void init_pwm()
     pwm_set_enabled(slice_blue, true);
 }
 
+void init_display()
+{
+    ssd1306_init(&ssd, WIDTH, HEIGHT, false, endereco, I2C_PORT);
+    ssd1306_config(&ssd);
+    ssd1306_send_data(&ssd);
+}
+
+void display_clear()
+{
+    ssd1306_fill(&ssd, false);
+    ssd1306_send_data(&ssd);
+}
+
+void update_display() {
+    ssd1306_fill(&ssd, !cor); // Limpa o display
+    ssd1306_rect(&ssd, 3, 3, 122, 60, cor, !cor); // Desenha um retângulo
+    ssd1306_line(&ssd, 3, 25, 123, 25, cor); // Desenha uma linha
+    ssd1306_line(&ssd, 3, 37, 123, 37, cor); // Desenha uma linha   
+    ssd1306_draw_string(&ssd, "CEPEDI   TIC37", 8, 6); // Desenha uma string
+    ssd1306_draw_string(&ssd, "EMBARCATECH", 20, 16); // Desenha uma string
+    ssd1306_draw_string(&ssd, "ADC   JOYSTICK", 10, 28); // Desenha uma string 
+    ssd1306_draw_string(&ssd, "X    Y    PB", 20, 41); // Desenha uma string    
+    ssd1306_line(&ssd, 44, 37, 44, 60, cor); // Desenha uma linha vertical         
+    ssd1306_draw_string(&ssd, adc_x, 8, 52); // Desenha uma string     
+    ssd1306_line(&ssd, 84, 37, 84, 60, cor); // Desenha uma linha vertical      
+    ssd1306_draw_string(&ssd, adc_y, 49, 52); // Desenha uma string   
+    ssd1306_rect(&ssd, 52, 90, 8, 8, cor, !gpio_get(JOYSTICK_BTN)); // Desenha um retângulo  
+    ssd1306_rect(&ssd, 52, 102, 8, 8, cor, !gpio_get(BUTTON_A)); // Desenha um retângulo    
+    ssd1306_rect(&ssd, 52, 114, 8, 8, cor, !cor); // Desenha um retângulo       
+    ssd1306_send_data(&ssd); // Atualiza o display
+}
+
 int main()
 {
     stdio_init_all();
@@ -90,6 +134,7 @@ int main()
     init_adc();
     init_i2c();
     init_pwm();
+    init_display();
 
 
     while (true) {
@@ -107,6 +152,15 @@ int main()
 
         printf("Joystick, eixo X: %d, eixo Y: %d\n", adc_x, adc_y);
 
-        sleep_ms(100);
+        if (pwm_enabled) {
+            pwm_set_gpio_level(LED_RED, adc_x);
+            pwm_set_gpio_level(LED_BLUE, adc_y);
+        } else {
+            pwm_set_gpio_level(LED_RED, 0);
+            pwm_set_gpio_level(LED_BLUE, 0);
+        }
+        
+
+        sleep_ms(20);
     }
 }
